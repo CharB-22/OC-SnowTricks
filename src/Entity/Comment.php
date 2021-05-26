f8cc223b6e587357f85fe3e02c945842a1791dfc
Charlène Bennevault <c.bennevault@gmail.com>
2021-05-26 10:28:10 +0200

    Create the comment entity

---
+++ src/Entity/Comment.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CommentRepository::class)
+ */
+class Comment
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $commentDate;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $commentContent;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCommentDate(): ?\DateTimeInterface
+    {
+        return $this->commentDate;
+    }
+
+    public function setCommentDate(\DateTimeInterface $commentDate): self
+    {
+        $this->commentDate = $commentDate;
+
+        return $this;
+    }
+
+    public function getCommentContent(): ?string
+    {
+        return $this->commentContent;
+    }
+
+    public function setCommentContent(string $commentContent): self
+    {
+        $this->commentContent = $commentContent;
+
+        return $this;
+    }
+}

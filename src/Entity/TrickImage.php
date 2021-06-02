eb14e3dbe6d215af28b861cbb715aae5e622a4b7
Charlène Bennevault <c.bennevault@gmail.com>
2021-06-02 09:37:10 +0200

    Create the Trick image entity

---
+++ src/Entity/TrickImage.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TrickImageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TrickImageRepository::class)
+ */
+class TrickImage
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $mediaName;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Trick::class, inversedBy="trickImages")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $trick;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMediaName(): ?string
+    {
+        return $this->mediaName;
+    }
+
+    public function setMediaName(string $mediaName): self
+    {
+        $this->mediaName = $mediaName;
+
+        return $this;
+    }
+
+    public function getTrick(): ?Trick
+    {
+        return $this->trick;
+    }
+
+    public function setTrick(?Trick $trick): self
+    {
+        $this->trick = $trick;
+
+        return $this;
+    }
+}

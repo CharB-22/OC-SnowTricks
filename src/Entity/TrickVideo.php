1da1c9bb8cb648f01d76d131f9ea229bd36192c9
Charlène Bennevault <c.bennevault@gmail.com>
2021-06-08 11:17:55 +0200

    Create the TrickVideo Form

---
+++ src/Entity/TrickVideo.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TrickVideoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TrickVideoRepository::class)
+ */
+class TrickVideo
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $videoName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $videoUrl;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Trick::class, inversedBy="trickVideos")
+     */
+    private $trick;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getVideoName(): ?string
+    {
+        return $this->videoName;
+    }
+
+    public function setVideoName(string $videoName): self
+    {
+        $this->videoName = $videoName;
+
+        return $this;
+    }
+
+    public function getVideoUrl(): ?string
+    {
+        return $this->videoUrl;
+    }
+
+    public function setVideoUrl(string $videoUrl): self
+    {
+        $this->videoUrl = $videoUrl;
+
+        return $this;
+    }
+
+    public function getTrick(): ?Trick
+    {
+        return $this->trick;
+    }
+
+    public function setTrick(?Trick $trick): self
+    {
+        $this->trick = $trick;
+
+        return $this;
+    }
+}

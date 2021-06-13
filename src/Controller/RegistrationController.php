4b31be7ddd0057180d96d6561868bbffc5deaa09
Charlène Bennevault <c.bennevault@gmail.com>
2021-06-13 17:45:14 +0200

    Translate some registration element in french

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -69,9 +69,9 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('noreply@test.com', 'SnowTricks'))
+                    ->from(new Address('noreply@snowtricks.com', 'SnowTricks'))
                     ->to($user->getEmail())
-                    ->subject('Please Confirm your Email')
+                    ->subject('Confirmation Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
             );
             // do anything else you need here, like send an email

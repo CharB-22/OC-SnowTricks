0bb2913e25e95e894103aa99f634d5c0859c7a44
Charlène Bennevault <c.bennevault@gmail.com>
2021-06-23 17:08:16 +0200

    Import bootstrap with Webpack Encore

--- assets/app.js
+++ assets/app.js
@@ -6,10 +6,11 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
-
+import './styles/app.scss';
+import { Tooltip, Toast, Popover } from 'bootstrap';
 // start the Stimulus application
 import './bootstrap'; 
+
 
 const newItem = (e) => {
     
